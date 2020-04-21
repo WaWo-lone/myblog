a78c245d3d56f092cf5e1f9847c056fcb0cbfdd6
caoyue <ningshisan13@gmail.com>
2020-04-21 11:03:55 +0800

    modify

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'l&58zxys&$(6b))@gy+c9m5(y*-rsk=ex@9rpw36h83ceb9t!m'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
