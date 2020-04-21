4fc35f0f8e6c7b3c0cec5ff8df29c2d8e598af64
caoyue <ningshisan13@gmail.com>
2020-04-21 10:34:43 +0800

    update

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'l&58zxys&$(6b))@gy+c9m5(y*-rsk=ex@9rpw36h83ceb9t!m'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
